6689c096e478e8e7ba32e12a4363469ae96b39cd
Bouji-dev <dev2024loper@gmail.com>
2025-01-15 21:00:40 +0330

    done

---
+++ src/constants.py
@@ -0,0 +1,5 @@
+CURRENCIES = [
+  'USD', 'EUR', 'GBP', 'JPY', 'INR', 'AUD', 'CAD', 'CHF', 'CNY', 'SEK', 'NZD', 'MXN',
+  'SGD', 'HKD', 'NOK', 'KRW', 'TRY', 'RUB', 'BRL', 'ZAR', 'MYR', 'THB', 'IDR', 'PHP',
+  'PLN', 'CZK', 'HUF', 'ILS', 'DKK', 'RON', 'CLP', 'PKR', 'VND', 'EGP', 'BDT', 'LKR'
+]
